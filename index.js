16be7049fb3c592c6e736d5fe87402f300273dfb
Adam <am1macdonald@gmail.com>
2022-10-01 19:24:56 -0400

    scaffold out server

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const http = require("http");
 const path = require("path");
+const fs = require("fs");
 
 const PORT = 8080;
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.writeHead(200, { "Content-Type": "text/html" });
+  const PATH = req.url;
+  console.log(PATH);
+  fs.readFile(path.join(__dirname, "public", `${PATH}.html`), (err, data) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.write(data);
+    return res.end(PATH);
+  });
 });
 
 server.listen(PORT);
